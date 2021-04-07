b457f8d80bc3e7bbc7dfbafda4a88d0c2892ac39
Gioele <gioele.armiento@gmail.com>
2021-04-07 16:38:03 +0200

    Next and prev functions added

--- js/script.js
+++ js/script.js
@@ -6,19 +6,31 @@ function initVue() {
 
         data: {
 
-            "test" : "Hello world!"
+            "test" : "Hello world!",
+            "images" : ["img/rathalos.webp", "img/rajang.webp", "img/zinogre.webp", "img/fatalis.webp"],    
+            "active": 0,
+            "max" : 3,
+            "min": 0
         },
 
         methods: {
 
             next: function() {
 
-                console.log("Next")
+                if (this.active >= this.max) {
+                    this.active = 0
+                } else {
+                    this.active++
+                }
             },
 
             prev: function() {
 
-                console.log("Prev")
+                if (this.active <= this.min) {
+                    this.active = this.max
+                } else {
+                    this.active--
+                }
             }
         }
     })
